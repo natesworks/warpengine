9cdabb12b5f56981bd33daea543d066c88599def
Natesworks <contact@natesworks.com>
2025-03-16 17:35:56 +0100

    :)

--- src/Rendering/Drawer.cpp
+++ src/Rendering/Drawer.cpp
@@ -13,11 +13,11 @@ void Drawer::drawLine(Point start, Point end, RGB color)
         return;
     }
 
-    std::cout << "[DEBUG] ScaleX: " << game->scaleX << "\n";
+    /*std::cout << "[DEBUG] ScaleX: " << game->scaleX << "\n";
     std::cout << "[DEBUG] ScaleY " << game->scaleY << "\n";
     std::cout << "[DEBUG] Line Start: {" << start.x << ", " << start.y << "}\n";
     std::cout << "[DEBUG] Line End: {" << end.x << ", " << end.y << "}\n";
-    std::cout << "[DEBUG] Line Color: {" << color.r << ", " << color.g << ", " << color.b << "}\n";
+    std::cout << "[DEBUG] Line Color: {" << color.r << ", " << color.g << ", " << color.b << "}\n";*/
 
     SDL_SetRenderDrawColor(game->renderer, color.r, color.g, color.b, 255);
     SDL_RenderDrawLine(game->renderer, start.x * game->scaleX, start.y * game->scaleY, end.x * game->scaleX, end.y * game->scaleY);
