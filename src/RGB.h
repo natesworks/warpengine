1bdc5ed08cc6b2f2dc46139153ece5ba2c003bc8
Natesworks <contact@natesworks.com>
2025-03-16 17:21:17 +0100

    .

--- src/RGB.h
+++ src/RGB.h
@@ -5,12 +5,7 @@
 class RGB
 {
 public:
-    RGB(uint8_t r, uint8_t g, uint8_t b)
-    {
-        this->r = r;
-        this->g = g;
-        this->b = b;
-    }
+    RGB(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {};
     uint8_t r;
     uint8_t g;
     uint8_t b;
--- src/Rendering/Drawer.cpp
+++ src/Rendering/Drawer.cpp
@@ -7,13 +7,22 @@
 
 void Drawer::drawLine(Point start, Point end, RGB color)
 {
-    std::cout << "[DEBUG] ScaleX " << game->scaleX << "\n[DEBUG] ScaleY " << game->scaleY << "\n";
+    if (!game)
+    {
+        std::cerr << "Game is not initialized!\n";
+        return;
+    }
+
+    std::cout << "[DEBUG] ScaleX: " << game->scaleX << "\n";
+    std::cout << "[DEBUG] ScaleY " << game->scaleY << "\n";
+    std::cout << "[DEBUG] Line Start: {" << start.x << ", " << start.y << "}\n";
+    std::cout << "[DEBUG] Line End: {" << end.x << ", " << end.y << "}\n";
+    std::cout << "[DEBUG] Line Color: {" << color.r << ", " << color.g << ", " << color.b << "}\n";
+
     SDL_SetRenderDrawColor(game->renderer, color.r, color.g, color.b, 255);
     SDL_RenderDrawLine(game->renderer, start.x * game->scaleX, start.y * game->scaleY, end.x * game->scaleX, end.y * game->scaleY);
     SDL_RenderPresent(game->renderer);
-    std::unique_ptr<Line> line;
-    line->color = color;
-    line->start = start;
-    line->end = end;
+
+    std::unique_ptr<Line> line = std::make_unique<Line>(start, end, color);
     game->objects.push_back(std::move(line));
 }
